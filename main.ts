33f3f8dfc17e484f727d92a6f2e52595d141753d
Doug Hooker <DouglassHooker@gmail.com>
2022-10-07 13:48:55 -0400

    Add sentry and better logging

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import { engine } from 'express-handlebars';
 import cookieParser from 'cookie-parser';
 import { PrismaClient } from '@prisma/client';
+import * as Sentry from '@sentry/node';
+import { captureException } from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import { hashPassword } from './lib/hashPassword';
 import { getJwt, getUserFromJwt } from './lib/getJwt';
@@ -10,11 +14,31 @@ console.log('🏖 Starting server...');
 
 const app = express();
 
+// Sentry
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.NODE_ENV || 'production',
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 0.5,
+});
+
 // Middleware
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use(cookieParser());
+if (process.env.NODE_ENV === 'development') {
+  // small logs for development
+  app.use(morgan('dev'));
+} else {
+  // large logs for production
+  app.use(morgan('combined'));
+}
 
 // Handlebars engine
 app.engine(
@@ -51,6 +75,7 @@ app.post('/users/new', async (req, res) => {
       .redirect('/links');
   } catch (e) {
     console.error(e);
+    captureException(e);
     res.sendStatus(400);
   }
 });
@@ -58,7 +83,6 @@ app.post('/users/new', async (req, res) => {
 app.get('/links', async (req, res) => {
   try {
     const cookie = req.cookies['poppin-tk'];
-    const prisma = new PrismaClient();
 
     if (!cookie) {
       res.redirect('/');
@@ -75,9 +99,13 @@ app.get('/links', async (req, res) => {
     }
   } catch (e) {
     console.error(e);
+    captureException(e);
     res.sendStatus(400);
   }
 });
+
+// Error handlers (must be after all routes)
+app.use(Sentry.Handlers.errorHandler());
 
 const port = process.env.PORT || 5000;
 app.listen(Number(port), '0.0.0.0', () => {
